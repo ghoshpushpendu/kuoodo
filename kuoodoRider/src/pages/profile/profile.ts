1d36d17ee8b830a8ea18d04b01828e0bef133b2b
Pushpendu Ghosh <info@gocubetech.com>
2019-04-02 11:40:05 +0530

    user profile picture updated

--- kuoodoRider/src/pages/profile/profile.ts
+++ kuoodoRider/src/pages/profile/profile.ts
@@ -1,86 +1,81 @@
 import { Component } from '@angular/core';
-import { NavController, IonicPage, NavParams, ActionSheetController, LoadingController, Loading } from 'ionic-angular';
-import { LocalStorageProvider } from '../../app.localStorage';
-import { AppService } from '../../app.providers';
+import { NavController, NavParams, LoadingController, ActionSheetController, Events, IonicPage } from 'ionic-angular';
 import { HttpService } from '../../app.httpService';
+import { AppService } from '../../app.providers';
+import { LocalStorageProvider } from '../../app.localStorage';
 import { Http, Headers, RequestOptions } from '@angular/http';
 import { Observable } from 'rxjs/Rx';
-import 'rxjs/add/operator/map';
-import 'rxjs/add/operator/catch';
-import { strings } from './../../lang';
 
-@IonicPage({ name: 'ProfilePage' })
+
+@IonicPage()
 @Component({
   selector: 'page-profile',
   templateUrl: 'profile.html',
   providers: [HttpService, AppService]
 })
 export class ProfilePage {
-  imageId: any;
-  image: void;
-  fileInfo: any;
-  url: any;
-  lastName: any;
-  firstName: any;
-  userID: any;
+  rate: any = 0;
 
+  // objectKeys = Object.keys;
+  // value = { option1: '+91', option2: '+93', option3: '+1' };
+
+  public user: any = {
+    address: {},
+  };
+
+  imageId: any = "";
+  public userImage = "https://via.placeholder.com/150?text=Click+To+Choose+Image";
+  lastName: string;
+  firstName: string;
   phoneNumber: any;
+  location: any;
   email: any;
-  username: any;
-
-  LoginId: any;
-  gId: any;
-  fbId: any;
+  username: string;
   id: any;
-  public string: any = strings;
 
   public isUsername: boolean = false;
   public isEmail: boolean = false;
-
-  public userImage = "https://openclipart.org/image/2400px/svg_to_png/190113/1389952697.png";
+  public isLocation: boolean = false;
 
   constructor(public navCtrl: NavController,
     public navParams: NavParams,
     private localStorageProvider: LocalStorageProvider,
     private httpService: HttpService,
+    private http: Http,
     private appService: AppService,
     public actionSheetCtrl: ActionSheetController,
-    private http: Http,
-    public loader: LoadingController) {
+    public loadingCtrl: LoadingController,
+    public events: Events) {
 
     /*
-  getting id's after completing registration
-  */
+    Getting id's after registration or login in driver profile page
+    */
 
     this.id = this.localStorageProvider.getUserId();
-
+    console.log("Getting Id in Profile page :");
+    console.log(this.id);
 
     /*
     Profile information 
     */
 
     if (this.id) {
-      let loading = this.loader.create({
-        content: this.string.fetchingProfile + " ..."
+      let loader = this.loadingCtrl.create({
+        content: 'Fetching profile... '
       });
-      loading.present();
+      loader.present();
       this.appService.getProfile(this.id, (error, data) => {
-        loading.dismiss()
+        loader.dismiss();
         if (error) {
           console.log("Error in fetching profile :", error);
         } else {
           if (data) {
             console.log("Profile information :");
             console.log(data);
-            this.isUsername = true;
-            this.isEmail = true;
-
-            this.username = data.user.firstName + " " + data.user.lastName;
-            this.email = data.user.email;
-            this.phoneNumber = data.user.phoneNumber;
+            this.user = data.user;
             this.imageId = data.user.profileImage;
             if (this.imageId) {
-              this.userImage = "http://mitapi.memeinfotech.com:5040/user/fileShow?imageId=" + this.imageId;
+              this.userImage = "https://kuoodo.snapbase.online/user/fileShow?imageId=" + this.imageId;
             }
           }
         }
@@ -89,12 +84,13 @@ export class ProfilePage {
   }
 
   /*
-  Edit profile 
-  */
+    Edit profile 
+    */
 
   editProfile() {
     this.isUsername = false;
     this.isEmail = false;
+    this.isLocation = false;
   }
 
   /*
@@ -102,23 +98,12 @@ export class ProfilePage {
   */
 
   submit() {
-    if (this.username) {
-      this.firstName = this.username.substr(0, this.username.indexOf(' '));
-      this.lastName = this.username.substr(this.username.indexOf(' ') + 1);
-      if (this.firstName.trim().length == 0) {
-        this.firstName = this.lastName;
-        this.lastName = "";
-      }
-    }
+    let _base = this;
 
     if (this.id) {
-      var data = {
-        _id: this.id,
-        firstName: this.firstName,
-        lastName: this.lastName,
-        email: this.email
-      }
-      this.appService.UpdateUser(data, (error, data) => {
+
+      this.user._id = this.id;
+      this.appService.UpdateUser(this.user, (error, data) => {
         if (error) {
           console.log("Update user data error :");
           console.log(error);
@@ -126,78 +111,69 @@ export class ProfilePage {
         else if (data) {
           console.log("Update user data :");
           console.log(data);
-          this.appService.updateUser(data.user);
+
+          this.username = data.user.firstName + " " + data.user.lastName;
+          this.email = data.user.email;
+          this.phoneNumber = data.user.phoneNumber;
+          this.location = data.user.location;
+          this.imageId = data.user.profileImage;
+          this.rate = data.user.rating;
+          if (this.imageId) {
+            this.userImage = "https://kuoodo.snapbase.online/user/fileShow?imageId=" + this.imageId;
+          }
+          // this.appService.updateUser(data.user);
+          _base.events.publish('data', data.user);
           this.isUsername = true;
           this.isEmail = true;
+          this.isLocation = true;
         }
       });
     }
   }
 
-  selectImage(event) {
-    let fileList: FileList = event.target.files;
-
-    console.log("File list information :");
-    console.log(fileList);
-
-    if (fileList.length > 0) {
-      let file: File = fileList[0];
-
-      if (file) {
-        var reader = new FileReader();
-
-        reader.onload = (event: any) => {
-          this.url = event.target.result;
-        }
-        reader.readAsDataURL(event.target.files[0]);
-      }
-
-      console.log("File information :");
-      console.log(file);
-
-      let formData: FormData = new FormData();
-      formData.append('file', file, file.name);
-
-      let headerOptions = new RequestOptions({
-        headers: new Headers({
-          'Accept': 'application/json'
-        }),
-      });
-
-
-      this.http.post(this.httpService.url + "user/fileUpload", formData, headerOptions)
-        .map(res => res.json())
-        .catch(error => Observable.throw(error))
-        .subscribe(
-          data => {
-            if (data) {
-              let id = data.upload._id;
-            }
-          },
-          error => {
-            if (error) {
-            }
-          });
-
-    }
-  }
-
-  updateUser(imageID: any) {
+  /*
+    Vehicle Insurance Upload
+  */
+  uploadImage(event) {
     let _base = this;
 
-    // let 
-    // return new Promise(function (resolve, reject) {
-    //   _base.appService.chargeCard(paymentData, function (error, data) {
-    //     if (error) {
-    //       reject(error);
-    //     }
-    //     else {
-    //       if (data) {
-    //         resolve(data);
-    //       }
-    //     }
-    //   });
-    // });
+    let fileList: FileList = event.target.files;
+    if (fileList.length > 0) {
+      let file: File = fileList[0];
+      if (file) {
+        var reader = new FileReader();
+        reader.onload = (event: any) => {
+          this.userImage = event.target.result;
+        }
+        reader.readAsDataURL(event.target.files[0]);
+        let formData: FormData = new FormData();
+        formData.append('file', file, file.name);
+
+        let headerOptions = new RequestOptions({
+          headers: new Headers({
+            'Accept': 'application/json'
+          }),
+        });
+        this.http.post(this.httpService.url + "user/fileUpload", formData, headerOptions)
+          .map(res => res.json())
+          .catch(error => Observable.throw(error))
+          .subscribe(
+            data => {
+              if (data) {
+                console.log(data);
+                let imageid = data.upload._id;
+                _base.user.profileImage = imageid;
+                _base.submit();
+              }
+            },
+            error => {
+              if (error) {
+                alert("Can not upload image");
+              }
+            })
+      }
+
+    }
   }
 
 
