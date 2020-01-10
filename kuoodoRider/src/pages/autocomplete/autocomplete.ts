5e177f6e64161beeb6d92df9c1937e8bfa1a8962
gocubetech <Info@gocubetech.com>
2020-01-10 08:43:56 +0530

    chnages

--- kuoodoRider/src/pages/autocomplete/autocomplete.ts
+++ kuoodoRider/src/pages/autocomplete/autocomplete.ts
@@ -1,4 +1,4 @@
-import { Component, NgZone } from '@angular/core';
+import { Component, ViewChild, NgZone, ElementRef } from '@angular/core';
 import { ViewController, NavController, IonicPage, NavParams } from 'ionic-angular';
 import { strings } from '../../lang';
 
@@ -19,6 +19,9 @@ export class AutocompletePage {
   latitude: number = 0;
   longitude: number = 0;
   geo: any
+  map: any;
+  @ViewChild('searchmap') mapElement: ElementRef;
+
 
   service = new google.maps.places.AutocompleteService();
 
@@ -37,6 +40,11 @@ export class AutocompletePage {
     if (elem) {
       elem.click();
     }
+
+
+    this.reziseMap()
+    this.initMap(this.param.get('lat'), this.param.get('lng'))
+
   }
 
   dismiss() {
@@ -84,11 +92,240 @@ export class AutocompletePage {
       let latitude = results[0].geometry.location.lat();
       let longitude = results[0].geometry.location.lng();
       let location = address;
-      _base.viewCtrl.dismiss({
-        lat: latitude,
-        lng: longitude,
-        location: location
-      });
+      // _base.viewCtrl.dismiss({
+      //   lat: latitude,
+      //   lng: longitude,
+      //   location: location
+      // });
     });
   }
+
+  reziseMap() {
+    let mapHeight = (<HTMLElement>document.getElementById("map")).clientHeight;
+    (<HTMLElement>document.getElementById("map")).style.height = mapHeight + "px";
+  }
+
+
+  //initialize map
+  initMap(lat, lng) {
+    let _base = this;
+
+    let mapStyle = [
+      {
+        "elementType": "geometry",
+        "stylers": [
+          {
+            "color": "#f5f5f5"
+          }
+        ]
+      },
+      {
+        "elementType": "labels.icon",
+        "stylers": [
+          {
+            "visibility": "off"
+          }
+        ]
+      },
+      {
+        "elementType": "labels.text.fill",
+        "stylers": [
+          {
+            "color": "#616161"
+          }
+        ]
+      },
+      {
+        "elementType": "labels.text.stroke",
+        "stylers": [
+          {
+            "color": "#f5f5f5"
+          }
+        ]
+      },
+      {
+        "featureType": "administrative",
+        "elementType": "geometry",
+        "stylers": [
+          {
+            "visibility": "off"
+          }
+        ]
+      },
+      {
+        "featureType": "administrative.land_parcel",
+        "elementType": "labels.text.fill",
+        "stylers": [
+          {
+            "color": "#bdbdbd"
+          }
+        ]
+      },
+      {
+        "featureType": "poi",
+        "stylers": [
+          {
+            "visibility": "off"
+          }
+        ]
+      },
+      {
+        "featureType": "poi",
+        "elementType": "geometry",
+        "stylers": [
+          {
+            "color": "#eeeeee"
+          }
+        ]
+      },
+      {
+        "featureType": "poi",
+        "elementType": "labels.text.fill",
+        "stylers": [
+          {
+            "color": "#757575"
+          }
+        ]
+      },
+      {
+        "featureType": "poi.park",
+        "elementType": "geometry",
+        "stylers": [
+          {
+            "color": "#e5e5e5"
+          }
+        ]
+      },
+      {
+        "featureType": "poi.park",
+        "elementType": "labels.text.fill",
+        "stylers": [
+          {
+            "color": "#9e9e9e"
+          }
+        ]
+      },
+      {
+        "featureType": "road",
+        "elementType": "geometry",
+        "stylers": [
+          {
+            "color": "#ffffff"
+          }
+        ]
+      },
+      {
+        "featureType": "road",
+        "elementType": "labels.icon",
+        "stylers": [
+          {
+            "visibility": "off"
+          }
+        ]
+      },
+      {
+        "featureType": "road.arterial",
+        "elementType": "labels.text.fill",
+        "stylers": [
+          {
+            "color": "#757575"
+          }
+        ]
+      },
+      {
+        "featureType": "road.highway",
+        "elementType": "geometry",
+        "stylers": [
+          {
+            "color": "#dadada"
+          }
+        ]
+      },
+      {
+        "featureType": "road.highway",
+        "elementType": "labels.text.fill",
+        "stylers": [
+          {
+            "color": "#616161"
+          }
+        ]
+      },
+      {
+        "featureType": "road.local",
+        "elementType": "labels.text.fill",
+        "stylers": [
+          {
+            "color": "#9e9e9e"
+          }
+        ]
+      },
+      {
+        "featureType": "transit",
+        "stylers": [
+          {
+            "visibility": "off"
+          }
+        ]
+      },
+      {
+        "featureType": "transit.line",
+        "elementType": "geometry",
+        "stylers": [
+          {
+            "color": "#e5e5e5"
+          }
+        ]
+      },
+      {
+        "featureType": "transit.station",
+        "elementType": "geometry",
+        "stylers": [
+          {
+            "color": "#eeeeee"
+          }
+        ]
+      },
+      {
+        "featureType": "water",
+        "elementType": "geometry",
+        "stylers": [
+          {
+            "color": "#c9c9c9"
+          }
+        ]
+      },
+      {
+        "featureType": "water",
+        "elementType": "labels.text.fill",
+        "stylers": [
+          {
+            "color": "#9e9e9e"
+          }
+        ]
+      }
+    ];
+
+    var uluru = { lat: lat, lng: lng };
+    let options = {
+      zoom: 15,
+      center: uluru,
+      disableDefaultUI: true,
+      // mapTypeId: 'roadmap',
+      // gestureHandling: 'cooperative',
+      zoomControl: true,
+      styles: mapStyle
+    };
+    this.map = new google.maps.Map(document.getElementById('searchmap'), options);
+
+    console.log("===================================================================")
+    console.log(this.map)
+
+    google.maps.event.addListener(_base.map, 'dragend', function () {
+      // do something only the first time the map is loaded
+      // _base.setCurrentLocation();
+      console.log(_base.map.getCenter())
+    });
+
+  }
+
 }
